d4f4f9f0933b1d9fa8002c59a8e53f39806b2d31
Marco <31511778+sysrqnl@users.noreply.github.com>
2017-09-01 11:11:09 +0200

    Support for multiple coins

--- sell_aid_script/index.js
+++ sell_aid_script/index.js
@@ -16,14 +16,7 @@ bittrex.options({
 
 const BASE_URL = "https://bittrex.com/api/v1.1/market/";
 
-var diff, amountBought, previousDiff;
-var buyActive = false;
-var buyPrice = 0;
-var maxDiff = -1000;
-
-var coin = process.argv[2];
-var amountBought;
-var sellPrice = 0;
+var coins = process.argv[2].split(',');
 
 bittrex.sendCustomRequest( "https://bittrex.com/api/v1.1/account/getbalances", function( data ) {
 
@@ -32,9 +25,9 @@ bittrex.sendCustomRequest( "https://bittrex.com/api/v1.1/account/getbalances", f
     }
     else {
       data.result.forEach(function(balance) {
-        // Only get history for the active coin
-        if (balance.Currency === coin) {
-          bittrex.sendCustomRequest( "https://bittrex.com/api/v1.1/account/getorderhistory?market=BTC-" + coin, function( data ) {
+        // Only get history for the active coins
+        if (coins.indexOf(balance.Currency) > -1) {
+          bittrex.sendCustomRequest( "https://bittrex.com/api/v1.1/account/getorderhistory?market=BTC-" + balance.Currency, function( data ) {
                 if (!data.success) {
                   return;
                 }
@@ -42,10 +35,18 @@ bittrex.sendCustomRequest( "https://bittrex.com/api/v1.1/account/getbalances", f
                   // Find the latest buy order
                   data.result.some(function(order) {
                     if (order.OrderType === "LIMIT_BUY") {
+                      var amountBought;
+                      var sellPrice = 0;
+                      var diff, previousDiff;
+                      var buyActive = false;
+                      var buyPrice = 0;
+                      var maxDiff = -1000;
+                      
                       amountBought = order.Quantity
                       buyPrice = order.PricePerUnit;
+                      log("Bought " + amountBought + " " + balance.Currency + " for " + buyPrice + " each.");
                       setInterval(function() {
-                        bittrex.sendCustomRequest( "https://bittrex.com/api/v1.1/public/getorderbook?market=BTC-" + coin + "&type=both", function( data ) {
+                        bittrex.sendCustomRequest( "https://bittrex.com/api/v1.1/public/getorderbook?market=BTC-" + balance.Currency + "&type=both", function( data ) {
                           // Start to calculate the dump price for the balance
                           var buyQuantity = 0;
                           if (!data.success) {
@@ -63,18 +64,18 @@ bittrex.sendCustomRequest( "https://bittrex.com/api/v1.1/account/getbalances", f
                           diff = calculateDiff(buyPrice, sellPrice);
                           // Substract the fees
                           diff -= 0.25;
-                          maxDiff = diff > maxDiff ? diff : maxDiff;
+                          maxDiff = diff > maxDiff ? diff : maxDiff;                          
                           // Only log if it changes
                           if (diff !== previousDiff) {
-                            log("New diff of " + diff + "%, max diff is " + maxDiff);
+                            log(balance.Currency + ": New diff of " + diff + "%, max diff is " + maxDiff);
+                            previousDiff = diff;
                           }
-                          previousDiff = diff;
                         });
                       }, 1000)
                       return true;
                     }
                   });
-                  log("Bought " + amountBought + " " + coin + " for " + buyPrice + " each.");
+                  
                 }
           }, true);
         }
