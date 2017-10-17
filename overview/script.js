1e2c6f1186bb44e1d6ce79d518f332a08dcafc4a
Huppeldeflut <31532077+Huppeldeflut@users.noreply.github.com>
2017-10-17 22:55:38 +0200

    Coin ignore improvements
    Cleaned up identation in code
    
    But most of all: added code for ignoring coins and reseting the filtering

--- overview/script.js
+++ overview/script.js
@@ -4,133 +4,144 @@ var bittrexApp = angular.module('bittrexApp', []);
 // create the controller and inject Angular's $scope
 bittrexApp.controller('mainController', function($rootScope, $http, $scope) {
 
-  var ignoreList = [];
-  var volumeMainLimit = 50; //coinTableMain
-  var volumeIntervalLimit = 50; //coinTableInterval (mustbe => volumeMainLimit)
-  var enableignoreList = false; //enable/disable ignore function
+	var minVolumeMain = 50;			//Minimum volume for coinTableMain
+	var minVolumeInterval = 50;		//Minimum volume for coinTableInterval (mustbe => minVolumeMain)
+	var maxItemsInterval = 5;		//Number of items per interval
+	
+	var logUpdateInterval = 10; 		//Every x seconds there is a log item
 
-  $rootScope.startTime = new Date().toISOString().slice(0, 19);
+	$rootScope.startTime = new Date().toISOString().slice(0, 19);
 
-  //Default sort on coinTableMain
-  $rootScope.orderByField = 'volumeDiff';
-  $rootScope.reverseSort =  true;
-  
-  if (localStorage["ignoreList"] === undefined) {
-  }
-  else {
-    ignoreList = localStorage["ignoreList"].split(",");
-  }
+	//Default sort on coinTableMain
+	$rootScope.orderByField = 'volumeDiff';
+	$rootScope.reverseSort =  true;
 
-  $rootScope.ignore = function(key) {
-    ignoreList.push(key);
-    localStorage.setItem("ignoreList", ignoreList.join(","));
-  };
+	var ignoreList = [];
+	if (localStorage["ignoreList"] === undefined) {
+		//ignoreList = undefined;
+	}
+	else {
+		ignoreList = localStorage["ignoreList"].split(",");
+	}
+	
+	$rootScope.ignore = function(key) {
+		ignoreList.push(key);
+		localStorage.setItem("ignoreList", ignoreList.join(","));
+	};
 
-  var backend = "http://34.240.107.131:1339"
-  var lastItems;
-  var coins = {};
-  var coin, startPrice, currentPrice, startVolume, currentVolume, hide;
-  var keys;
-  var topIntervals = [1,5,15,30, 60, 240];
-  $rootScope.intervals = topIntervals;
-  var tops = {};
+	$rootScope.resetHide = function(){
+		localStorage.removeItem("ignoreList");
+		ignoreList = [];
+	}
+	
+	var backend = "http://34.240.107.131:1339"
+	var lastItems;
+	var coins = {};
+	var coin, startPrice, currentPrice, startVolume, currentVolume, hide;
+	var keys;
+	var topIntervals = [1,5,15,30, 60, 240];
+	$rootScope.intervals = topIntervals;
+	var tops = {};
 
-  $rootScope.finalList = [];
+	$rootScope.finalList = [];
+	
+	$http.get(backend).
+		then(handleResponse);
 
-  $http.get(backend).
-    then(handleResponse);
+	function handleResponse(response) {
+			if (response.data && response.data.success) {
+				lastItems = response.data.result.filter(function(item) {
+				return item.MarketName.indexOf("BTC-") > -1;
+			});
 
-  function handleResponse(response) {
-    if (response.data && response.data.success) {
-      lastItems = response.data.result.filter(function(item) {
-        return item.MarketName.indexOf("BTC-") > -1;
-      });
+			lastItems = lastItems.filter(function(item) {
+				return item.BaseVolume > minVolumeMain;
+			});
 
+			topIntervals.forEach(function(top) {
+				tops[top] = [];
+			});
 
-      lastItems = lastItems.filter(function(item) {
-		if (enableignoreList == true) {
-			return ignoreList.indexOf(item.MarketName.replace("BTC-", "")) == -1;
-		}
-		else {
-			return true;
-		};
-      });
-
-      lastItems = lastItems.filter(function(item) {
-        return item.BaseVolume > volumeMainLimit;
-      });
-
-      topIntervals.forEach(function(top) {
-        tops[top] = [];
-      });
-
-      lastItems.forEach(function(item) {
-        coin = item.MarketName.replace("BTC-", "");
-        if (!coins[coin]) {
-          coins[coin] = {
-            priceLog : [],
-            volumeLog : []
-          };
-        }
-        coins[coin].priceLog.push(item.Bid.toFixed(8));
-        coins[coin].volumeLog.push(item.BaseVolume.toFixed(0));
-
-      });
-      keys = Object.keys(coins);
-      $rootScope.finalList = [];
-
-      keys.forEach(function(key) {
-        currentPrice = coins[key].priceLog[coins[key].priceLog.length - 1];
-        startPrice = coins[key].priceLog[0];
-        currentVolume = coins[key].volumeLog[coins[key].volumeLog.length - 1];
-        startVolume = coins[key].volumeLog[0];
-
-			$rootScope.finalList.push({
-			  coin: key,
-			  start: startPrice,
-			  price: currentPrice,
-			  volume: Number(currentVolume),
-			  diffSinceStart: Number(((currentPrice * 100) / startPrice - 100).toFixed(1)),
-			  volumeDiff: Number(((currentVolume * 100) / startVolume - 100).toFixed(1)),
+			lastItems.forEach(function(item) {
+				coin = item.MarketName.replace("BTC-", "");
+				if (!coins[coin]) {
+					coins[coin] = {
+						priceLog : [],
+						volumeLog : []
+					};
+				}
+				coins[coin].priceLog.push(item.Bid.toFixed(8));
+				coins[coin].volumeLog.push(item.BaseVolume.toFixed(0));
+				
 			});
 			
-			topIntervals.forEach(function(top) {
-			  if (coins[key].priceLog.length > (top * 60) / 5 ) {
-				startPrice = coins[key].priceLog[coins[key].priceLog.length - (top * 60) / 5];
-				startVolume = coins[key].volumeLog[coins[key].volumeLog.length - (top * 60) / 5];
-				
-				if (coins[key].volumeLog[coins[key].volumeLog.length - 1] > volumeIntervalLimit) {
-				  tops[top].push({
-					coin: key,
-					start: startPrice,
-					price: currentPrice,
-					volume: Number(currentVolume),
-					diffSinceStart: Number(((currentPrice * 100) / startPrice - 100).toFixed(1)),
-					volumeDiff: Number(((currentVolume * 100) / startVolume - 100).toFixed(1))
-				  })
-				}
-			  };
-			});
-      });
-      $rootScope.finalList.sort(function(a, b) {
-        return b.diffSinceStart - a.diffSinceStart;
-      });
-      $rootScope.tops = {};
-      topIntervals.forEach(function(top) {
-        if (tops[top].length) {
-          tops[top].sort(function(a, b) {
-            return b.diffSinceStart - a.diffSinceStart;
-          });
-          $rootScope.tops[top] = tops[top].slice(0,5);
-        }
-      });
-    }
-  }
+			keys = Object.keys(coins);
+			$rootScope.finalList = [];
+			
+			//Fill / Update the ingnoreList in the rootScope for display on page
+			$rootScope.ignoreList = localStorage["ignoreList"];
+			
+			//Cycle through the coins
+			keys.forEach(function(key) {
+				if (ignoreList.indexOf(key) == -1){
+					//Coin NOT in ignore list
+					currentPrice = coins[key].priceLog[coins[key].priceLog.length - 1];
+					startPrice = coins[key].priceLog[0];
+					currentVolume = coins[key].volumeLog[coins[key].volumeLog.length - 1];
+					startVolume = coins[key].volumeLog[0];
 
-  setInterval(function() {
-    $http.get(backend).
-      then(handleResponse);
-  }, 10000);
+					$rootScope.finalList.push({
+						coin: key,
+						start: startPrice,
+						price: currentPrice,
+						volume: Number(currentVolume),
+						diffSinceStart: Number(((currentPrice * 100) / startPrice - 100).toFixed(1)),
+						volumeDiff: Number(((currentVolume * 100) / startVolume - 100).toFixed(1)),
+					});
+					
+					topIntervals.forEach(function(top) {
+						var itemsPerLogInterval = (top * 60) / logUpdateInterval;
+						
+						if (coins[key].priceLog.length > (top * 60) / logUpdateInterval ) {
+							startPrice = coins[key].priceLog[coins[key].priceLog.length - itemsPerLogInterval];
+							startVolume = coins[key].volumeLog[coins[key].volumeLog.length - itemsPerLogInterval];
+							
+							if (coins[key].volumeLog[coins[key].volumeLog.length - 1] > minVolumeInterval) {
+								tops[top].push({
+									coin: key,
+									start: startPrice,
+									price: currentPrice,
+									volume: Number(currentVolume),
+									diffSinceStart: Number(((currentPrice * 100) / startPrice - 100).toFixed(1)),
+									volumeDiff: Number(((currentVolume * 100) / startVolume - 100).toFixed(1))
+								})
+							}
+						};
+					});
+				}else{
+					//Coin in ignore list
+				}
+			});
+			$rootScope.finalList.sort(function(a, b) {
+				return b.diffSinceStart - a.diffSinceStart;
+			});
+			
+			$rootScope.tops = {};
+			topIntervals.forEach(function(top) {
+				if (tops[top].length) {
+					tops[top].sort(function(a, b) {
+						return b.diffSinceStart - a.diffSinceStart;
+					});
+					$rootScope.tops[top] = tops[top].slice(0,maxItemsInterval);
+				}
+			});
+			}
+	}
+
+	setInterval(function() {
+		$http.get(backend).
+			then(handleResponse);
+	}, 10000);
 
 
 });
@@ -147,3 +158,4 @@ bittrexApp.directive('coinTableInterval', function() {
     templateUrl: "table-interval.html"
   };
 });
+
