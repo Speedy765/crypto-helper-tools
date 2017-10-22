879d391a18577a44e49c4a27b62bc593408b5b0a
Huppeldeflut <31532077+Huppeldeflut@users.noreply.github.com>
2017-10-22 20:37:29 +0200

    Sorting and Debug option added
    Added requested sorting.
    
    For my self and other programmers:
    Added a parameter to enable a debug mode.
    In this mode a snapshot of the data is stored in the localstorage.
    This way the backend server doesn't get overwelmed with requests.

--- long/script.js
+++ long/script.js
@@ -7,10 +7,14 @@ bittrexApp.controller('mainController', function($rootScope, $http, $scope) {
 	var intervals = [2,4,8,16,32,64, 128, 256, 512];	//Interval for interval list
 
 	$rootScope.headers = intervals;
+	
+	//Use localstorage for debuging
+	var debug = false;
+	
 	//Default sort on coinTableMain
-	$rootScope.orderByField = 'volumeDiff';
-	$rootScope.reverseSort =  true;
-
+	$rootScope.orderByField = 'coin';
+	$rootScope.reverseSort =  false;
+	
 	var ignoreList = [];
 	if (localStorage["ignoreList"] === undefined) {
 		//ignoreList = undefined;
@@ -39,10 +43,35 @@ bittrexApp.controller('mainController', function($rootScope, $http, $scope) {
 	var tops = {};
 
 
-
-	$http.get(backend).
-		then(handleResponse);
-
+	//If debug is true, then chech if data is stored in localstorage
+	//If needed store data in localStorage
+	//Then run the code with the localy stored data
+	//
+	//If debug is false, then use online data
+	if (debug){
+		var response1 = '';
+		if (localStorage["testjes"] === undefined) {
+			//Get data online and store this localy befor running the rest of the code
+			$http.get(backend).then(function(x){
+				response1 = {data: x.data};
+				localStorage["testjes"] = JSON.stringify(x.data);
+				handleResponse(response1);
+			});
+			
+		}else{
+			//Data is already localy, so run with this data
+			response1 = {
+				data: JSON.parse(localStorage["testjes"])
+			};
+			handleResponse(response1);
+		};
+		
+	} else {
+		//Get data online
+		$http.get(backend).
+			then(handleResponse);
+	}
+	
 	function handleResponse(response) {
 		if (response.data && response.data.ETH.log.length) {
 			var coins = Object.keys(response.data);
@@ -50,7 +79,13 @@ bittrexApp.controller('mainController', function($rootScope, $http, $scope) {
 			var coinData;
 			coins.forEach(function(key) {
 				coinData = response.data[key];
-				$rootScope.finalList.push(coinData);
+				$rootScope.finalList.push({
+					coin: coinData.coin,
+					current: coinData.current.toFixed(8),
+					high: coinData.high.toFixed(8),
+					low: coinData.low.toFixed(8),
+					log: coinData.log.map(Number),
+				});
 			});
 		}
 	}
