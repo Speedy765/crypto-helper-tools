8d93c0c0f06061864367d19d1725a92bfdf62a04
Martijn de Jongh <martijn_de_jongh@hotmail.com>
2017-12-31 13:12:48 +0100

    0.4 multi market overview

--- app/main/main.js
+++ app/main/main.js
@@ -1,6 +1,6 @@
 
 var cryptotracky = angular.module('cryptotracky', ['ui.router', "chart.js", "LocalStorageModule"]);
-var version = "0.3";
+var version = "0.4";
 
 cryptotracky.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider.state({
